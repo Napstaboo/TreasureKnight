a489724c6b69ee56172f0f2abaab6d30329fa0e2
Napstaboo <anbahalath@gmail.com>
2025-05-16 02:24:25 -0700

    added menu, folder memory, folder updates

--- filesystem/script.js
+++ filesystem/script.js
@@ -4,22 +4,33 @@ folders = []
 class folder{
   constructor(name){
     this.name = name;
-    const elem = document.createElement("div");
-    elem.className = "folder";
-    elem.id = name;
-    elem.innerHTML=name;
-    elem.addEventListener("click", () => {this.open("open")});
-    document.getElementById("window").appendChild(elem);
     this.fileList = [];
     folders.push(this);
+    this.create();
+  }
+
+  create(){
+    const elem = document.createElement("div");
+    elem.className = "folder";
+    elem.id = this.name;
+    elem.innerHTML = this.name;
+    elem.addEventListener("click", () => {this.open(this.name)});
+    document.getElementById("window").appendChild(elem);
   }
 
   append(file){
     this.fileList.push(file);
+    update(this.name, this);
+    //console.log(folders[0].fileList);
   }
 
-  open(string){
-    console.log(string);
+  open(name){
+    console.log(name);
+    var win = document.getElementById("window");
+    win.innerHTML = "";
+    this.fileList.forEach((file, index) => {
+      file.create(win);
+    })
   }
 
   //function to replace window innerhtml with files
@@ -35,27 +46,41 @@ class file {
   }
 
   create(win){
-    const elem = win.createElement("div");
-    elem.className("file");
+    const elem = document.createElement("div");
+    elem.className = "file";
     elem.id = `${this.name}.${this.extension}`
     elem.innerText = `${this.name}.${this.extension}`
     elem.draggable = true;
+    win.appendChild(elem);
   }
 }
+
+function update(name, content){
+let i = folders.findIndex((folder, index, array) => {
+  folder.name = name;
+});
+
+if(i == -1){
+  folders.push(content);
+} else {folders.splice(i, 1, content)};
+
+}
+
+function menu(){
+  var win = document.getElementById("window");
+  win.innerHTML = "";
+  folders.forEach((folder, index) => {
+    folder.create();
+  })
+}
+
 
 //list of folders. click on a folder replaces innerhtml with files in folder.
 //permanent button to return to main directory
 //ondragdrop, get id of dropped file. create a new file with name and append to folder.
 //on folder click, replace innerhtml with the file innertext
 
-new folder("main");
-new folder("bruh");
-new folder("main");
-new folder("bruh");
-new folder("noob");
-new folder("main");
-new folder("grace");
-new folder("bruh");
-new folder("main");
-new folder("love");
-new folder("bruh");
+var main = new folder("main");
+var file1 = new file("file1", "txt");
+//console.log(folders[0].fileList)
+main.append(file1);
