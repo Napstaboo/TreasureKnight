0753fc5b101befabe0ede22f9e206d90bbcaf09f
Napstaboo <anbahalath@gmail.com>
2025-05-18 23:38:53 -0700

    filesystem

--- browser/sketch.js
+++ browser/sketch.js
@@ -5,41 +5,9 @@ function setup() {
   searchBar.position(0.025*windowWidth, 20);
   searchBar.size(0.95*windowWidth, 25)
 
-  guy1 = new draggableGuy(10, 10, 200, 300)
-  draggables.push(guy1)
 }
 
 function draw() {
-  draggables.forEach(draggable => {
-    draggable.render();
-    //console.log(draggable.xPos)
-  });
-}
-
-function mouseDragged() {
-  draggables.forEach(draggable => {
-    draggable.move(mouseX, mouseY);
-    //console.log(draggable.xPos);
-  });
-}
-
-class draggableGuy {
-  constructor(x, y, height, width){
-    this.xPos = x;
-    this.yPos = y;
-    this.height = height;
-    this.width = width;
-  }
-
-  move(x, y){
-    //console.log(this.xPos)
-    this.xPos = x;
-    this.yPos = y;
-  }
-
-  render() {
-    rect(this.xPos, this.yPos, this.height, this.width);
-    console.log(this.xPos)
-  }
 
 }
+
--- filesystem/script.js
+++ filesystem/script.js
@@ -1,21 +1,29 @@
 folders = []
-
+selected = []
 
 class folder{
   constructor(name){
-    this.name = name;
-    this.fileList = [];
-    folders.push(this);
-    this.create();
+    let i = folders.findIndex((folder, index, array) => {
+    return folder.name == name;
+    });
+    if (i == -1){
+      this.name = name;
+      this.fileList = [];
+      folders.push(this);
+      this.create();
+    }
+    else{
+      new folder(`${name}(1)`)
+    }
   }
 
   create(){
     const elem = document.createElement("div");
     elem.className = "folder";
     elem.id = this.name;
-    elem.innerHTML = this.name;
+    elem.innerHTML = `<div class="label">${this.name}</div>`;
     elem.addEventListener("click", () => {this.open(this.name)});
-    document.getElementById("window").appendChild(elem);
+    document.getElementById("window-content").appendChild(elem);
   }
 
   append(file){
@@ -26,11 +34,17 @@ class folder{
 
   open(name){
     console.log(name);
-    var win = document.getElementById("window");
+    var win = document.getElementById("window-content");
     win.innerHTML = "";
     this.fileList.forEach((file, index) => {
       file.create(win);
     })
+
+    var newButton = document.getElementById("new item");
+    newButton.removeEventListener('click', newFolder);
+    newButton.param = this;
+    newButton.addEventListener('click', newFile);
+
   }
 
   //function to replace window innerhtml with files
@@ -56,23 +70,41 @@ class file {
 }
 
 function update(name, content){
-let i = folders.findIndex((folder, index, array) => {
-  folder.name = name;
-});
+  let i = folders.findIndex((folder, index, array) => {
+    return folder.name == name;
+  });
+  
+  console.log(i);
+  if(i == -1){
+    folders.push(content);
+  } else {folders.splice(i, 1, content)};
 
-if(i == -1){
-  folders.push(content);
-} else {folders.splice(i, 1, content)};
+};
 
+function newFolder(event){
+  fol = new folder("newFolder");
 }
 
-function menu(){
+function newFile(event){
+  fil = new file("new file", 'txt');
+  event.currentTarget.param.append(fil);
+  event.currentTarget.param.open();
+}
+
+function showWindow(){
   var win = document.getElementById("window");
-  win.innerHTML = "";
+  win.style.display = "block";
+  var content = document.getElementById("window-content")
+  content.innerHTML = "";
   folders.forEach((folder, index) => {
     folder.create();
   })
-}
+  var newButton = document.getElementById("new item");
+  newButton.removeEventListener('click', newFile);
+  newButton.addEventListener('click', newFolder);
+};
+
+
 
 
 //list of folders. click on a folder replaces innerhtml with files in folder.
@@ -82,5 +114,15 @@ function menu(){
 
 var main = new folder("main");
 var file1 = new file("file1", "txt");
-//console.log(folders[0].fileList)
+console.log(folders)
 main.append(file1);
+console.log(folders);
+
+
+
+//browser
+//filesystem
+  //system32, downloads, GAME/ISO/CHARACTERS
+//emulator
+  //files, keyboard driver
+//game
